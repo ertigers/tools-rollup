71dc487cb7fce47773c4c84bf59c9076ff79df5c
lvyihu <1720427782@qq.com>
2021-12-20 18:26:06 +0800

    更新12-20

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import { terser } from "rollup-plugin-terser"
 export default {
   input: 'lib/webcu2plugin.js',
   output: [{
-    name: 'Gulu',
+    name: 'QX',
     file: 'dist/webcu2plugin.js',
     format: 'umd',
     plugins: [terser()]

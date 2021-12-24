2ec6c6290a096e325b83c523dde638c037ab0f3c
lvyihu <1720427782@qq.com>
2021-12-24 18:54:20 +0800

    12.24-1

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import { terser } from "rollup-plugin-terser"
 
 export default {
   input: 'lib/webcu2plugin.js',
+  // input: 'lib/min/webcu2plugin.js',
   output: [{
     name: 'QX',
     file: 'dist/webcu2plugin.js',
